c99e265fe60994114e8d616f0edae7362cd892ee
Alessandra Canuto <ale.canutto@gmail.com>
2023-09-09 12:00:36 -0300

    feat: add sidebar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -2,11 +2,12 @@
 
 import { cn } from '@/lib/utils'
 import { UserButton } from '@clerk/nextjs'
-import { Menu, Sparkles } from 'lucide-react'
-import { Poppins } from 'next/font/google'
 import Link from 'next/link'
+import { Sparkles } from 'lucide-react'
+import { Poppins } from 'next/font/google'
 import { Button } from '@/components/ui/button'
 import { ModeToggle } from './mode-toggle'
+import { MobileSidebar } from './mobile-sidebar'
 
 const font = Poppins({
   weight: '600',
@@ -15,9 +16,9 @@ const font = Poppins({
 
 export const Navbar = () => {
   return (
-    <div className="fixed w-full z-50 flex justify-between items-center py-2 px-4 border-b border-primary/10 bg-secondary">
+    <div className="fixed w-full z-50 flex justify-between items-center py-2 px-4 border-b border-primary/10 bg-secondary h-16">
       <div className="flex items-center">
-        <Menu className="block md:hidden" />
+        <MobileSidebar />
         <Link href="/">
           <h1
             className={cn(
