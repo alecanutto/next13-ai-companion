2c06b1d1df08540d96026a603aaea1de0ce83e33
Alessandra Canuto <ale.canutto@gmail.com>
2023-09-09 09:39:08 -0300

    folder setup

---
+++ app/(auth)/(routes)/login/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LoginPage = () => {
+  return (
+    <div>Login Page (UnProtected)</div>
+  )
+}
+
+export default LoginPage
---
+++ app/(root)/(routes)/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const RootPage = () => {
+  return (
+    <div>Root Page (Protected)</div>
+  )
+}
+
+export default RootPage
