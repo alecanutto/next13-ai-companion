082ddf641a6583c3cf4600f867979135a4629e5d
Alessandra Canuto <ale.canutto@gmail.com>
2023-09-09 12:46:27 -0300

    feat: add search filter

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,7 +1,11 @@
-import React from 'react'
+import SearchInput from '@/components/search-input'
 
 const RootPage = () => {
-  return <div>Root Page</div>
+  return (
+    <div className="h-full p-4 space-y-2">
+      <SearchInput />
+    </div>
+  )
 }
 
 export default RootPage
