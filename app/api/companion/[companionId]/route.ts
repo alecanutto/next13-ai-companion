ef5028c210454408dbc9c426b27bd0d7f77ae065
Alessandra Canuto <ale.canutto@gmail.com>
2023-09-10 18:02:08 -0300

    feat: add chat header

--- app/api/companion/[companionId]/route.ts
+++ app/api/companion/[companionId]/route.ts
@@ -1,4 +1,4 @@
-import { currentUser } from '@clerk/nextjs'
+import { auth, currentUser } from '@clerk/nextjs'
 import { NextResponse } from 'next/server'
 
 import prismadb from '@/lib/prismadb'
@@ -51,6 +51,31 @@ export async function PATCH(
     return NextResponse.json(companion)
   } catch (error) {
     console.log('[COMPANION_PATCH]', error)
+    return new NextResponse('Internal Error', { status: 500 })
+  }
+}
+
+export async function DELETE(
+  request: Request,
+  { params }: { params: { companionId: string } },
+) {
+  try {
+    const { userId } = auth()
+
+    if (!userId) {
+      return new NextResponse('Unauthorized', { status: 401 })
+    }
+
+    const companion = await prismadb.companion.delete({
+      where: {
+        userId,
+        id: params.companionId,
+      },
+    })
+
+    return NextResponse.json(companion)
+  } catch (error) {
+    console.log('[COMPANION_DELETE]', error)
     return new NextResponse('Internal Error', { status: 500 })
   }
 }
